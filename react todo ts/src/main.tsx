c211f5c0b394f0d3feab99a3e840863c1cba80db
erickubuntu <gestionecesaria@gmail.com>
2023-12-13 15:45:26 -0400

    todo terminado

--- react todo ts/src/main.tsx
+++ react todo ts/src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { TodoApp } from "./components/TodoApp";
-// import "./styles.css";
+import "./styles.css";
+
+//BOOTSTRAP
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
